1af6e47f7de4ea6aa840ad0e519b0af105ce03aa
OluwaseunOdufisan <odufisanoluwaseunisaac@gmail.com>
2022-06-29 13:46:13 +0100

    Spring Data and Rest controller API web app

---
+++ SpringDataRest/src/main/java/com/karlearnder/demo/AlienRepo.java
@@ -0,0 +1,12 @@
+package com.karlearnder.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.karlearnder.demo.model.Alien;
+
+
+@RepositoryRestResource(collectionResourceRel="aliens", path="aliens")
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
+
+}
---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101, 'Isaac','java');
+insert into alien values (102, 'Odus', 'spring');
+insert into alien values (103, 'Oluwaseun', 'java');
+insert into alien values (104, 'Karl', 'mvc');
+insert into alien values (105, 'Earnder', 'java');
